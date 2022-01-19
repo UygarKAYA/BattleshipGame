88439cc6d55bfb92007368a5aaa69f979b8db79f
Uygar Kaya <uygar.kaya@ozu.edu.tr>
2022-01-19 16:29:24 +0300

    Add files via upload

---
+++ BattleshipGame/src/GameModels/Carrier.java
@@ -0,0 +1,13 @@
+package GameModels;
+
+public class Carrier extends NavalShips {
+    private static final int size = 5;
+    private static final String sign = "C";
+
+    public int getSize() {
+        return size;
+    }
+    public String getSign() {
+        return sign;
+    }
+}
