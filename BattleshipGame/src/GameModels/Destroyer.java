f48eb0ef17c5bab36124832e5886ca345f71cba2
Uygar Kaya <uygar.kaya@ozu.edu.tr>
2022-01-19 16:29:41 +0300

    Add files via upload

---
+++ BattleshipGame/src/GameModels/Destroyer.java
@@ -0,0 +1,14 @@
+package GameModels;
+
+public class Destroyer extends NavalShips {
+    private static final int size = 2;
+    private static final String sign = "D";
+
+    public int getSize() {
+        return size;
+    }
+    public String getSign() {
+        return sign;
+    }
+}
+
